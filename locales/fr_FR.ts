195cf4b73802cd437f6bbb8f6e92886778c453fa
Skydreamer <devnull@localhost>
2009-12-05 23:34:14 +0100

    Traductions

--- locales/fr_FR.ts
+++ locales/fr_FR.ts
@@ -1,29 +1,29 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="fr_FR" sourcelanguage="en">
 <context>
     <name>Log</name>
     <message>
         <location filename="../src/core/Log.cpp" line="26"/>
         <source>dd/MM/yy</source>
-        <translation type="unfinished"></translation>
+        <translation>dd/MM/yy</translation>
     </message>
     <message>
         <location filename="../src/core/Log.cpp" line="27"/>
         <source>hh:mm</source>
-        <translation type="unfinished"></translation>
+        <translation>hh:mm</translation>
     </message>
     <message>
         <location filename="../src/core/Log.cpp" line="35"/>
         <source>Information</source>
         <comment>From log</comment>
-        <translation type="unfinished"></translation>
+        <translation>Information</translation>
     </message>
     <message>
         <location filename="../src/core/Log.cpp" line="40"/>
         <source>Error</source>
         <comment>From log</comment>
-        <translation type="unfinished"></translation>
+        <translation>Erreur</translation>
     </message>
 </context>
 <context>
@@ -31,7 +31,7 @@
     <message>
         <location filename="../src/core/LogParser.cpp" line="13"/>
         <source>Server&apos;s log not found (%1).</source>
-        <translation type="unfinished"></translation>
+        <translation>Log du serveur non trouvé (%1).</translation>
     </message>
 </context>
 <context>
@@ -39,68 +39,69 @@
     <message>
         <location filename="../src/modules/Admin.cpp" line="5"/>
         <source>admintest</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="7"/>
         <source>Displays your admin level.</source>
-        <translation type="unfinished"></translation>
+        <translation>Affiche votre niveau d&apos;admin.</translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="9"/>
         <source>kick</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="10"/>
         <location filename="../src/modules/Admin.cpp" line="14"/>
         <source>[^3name^7|^3id^7]</source>
-        <translation type="unfinished"></translation>
+        <translation>[^3nom^7|^3id^7]</translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="11"/>
         <source>Kicks a player from the server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ejecte un joueur du serveur.</translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="13"/>
         <source>mute</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="15"/>
         <source>Mutes a player.</source>
-        <translation type="unfinished"></translation>
+        <translation>Rend un joueur muet.</translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="17"/>
         <source>readconfig</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="19"/>
         <source>Reloads admins and levels.</source>
-        <translation type="unfinished"></translation>
+        <translation>Recharge els administrateurs et les niveaux.</translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="59"/>
         <source>^3!%1^7 : Access denied.</source>
-        <translation type="unfinished"></translation>
+        <translation>^3!%1^7 : Acces interdit.</translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="69"/>
         <source>Syntax : ^3!%1^7 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Syntaxe : ^3!%1^7 %2</translation>
     </message>
     <message>
         <location filename="../src/modules/Admin.cpp" line="126"/>
         <source>^3!admintest^7: %1^7 is a %2^7 (level %3).</source>
-        <translation type="unfinished"></translation>
+        <translation>^3!admintest^7: %1^7 est un  %2^7 (niveau %3).</translation>
     </message>
     <message numerus="yes">
         <location filename="../src/modules/Admin.cpp" line="179"/>
         <source>^3!readconfig^7: %1 levels</source>
         <translation type="unfinished">
+            <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
     </message>
@@ -108,6 +109,7 @@
         <location filename="../src/modules/Admin.cpp" line="181"/>
         <source>and %2 admins loaded.</source>
         <translation type="unfinished">
+            <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
     </message>
@@ -118,18 +120,18 @@
         <location filename="../src/modules/Player.cpp" line="59"/>
         <location filename="../src/modules/Player.cpp" line="76"/>
         <source>No players found.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aucun joueur trouve.</translation>
     </message>
     <message>
         <location filename="../src/modules/Player.cpp" line="84"/>
         <source>Several players found, please use ID: </source>
-        <translation type="unfinished"></translation>
+        <translation>Plusieurs joueurs trouves, utilisez l&apos;ID :</translation>
     </message>
     <message>
         <location filename="../src/modules/Player.cpp" line="86"/>
         <source>%1 =&gt; %2^7, </source>
         <comment>id =&gt; name player list</comment>
-        <translation type="unfinished"></translation>
+        <translation>%1 =&gt; %2^7</translation>
     </message>
 </context>
 <context>
@@ -137,7 +139,7 @@
     <message>
         <location filename="../src/core/Log.cpp" line="25"/>
         <source>[%1 %2] %3 : %4</source>
-        <translation type="unfinished"></translation>
+        <translation>[%1 %2] %3 : %4</translation>
     </message>
 </context>
 <context>
@@ -145,12 +147,12 @@
     <message>
         <location filename="../src/core/Server.cpp" line="17"/>
         <source>Connected to %1 on port %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>Connecté à %1 sur le port %2.</translation>
     </message>
     <message>
         <location filename="../src/core/Server.cpp" line="43"/>
         <source>Could not connect to %1 on port %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>Connexion impossible à %1 sur le port %2.</translation>
     </message>
 </context>
 <context>
@@ -159,12 +161,12 @@
         <location filename="../src/core/zUrt.cpp" line="11"/>
         <source>Hi everyone !</source>
         <comment>Bot greeting when it connects</comment>
-        <translation type="unfinished"></translation>
+        <translation>Salut tout le monde !</translation>
     </message>
     <message>
         <location filename="../src/core/zUrt.cpp" line="44"/>
         <source>Loading %1 module.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chargement du module %1.</translation>
     </message>
 </context>
 </TS>
